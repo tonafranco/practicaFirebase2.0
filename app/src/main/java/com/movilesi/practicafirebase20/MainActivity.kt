89c5b83e49fb99d3e99c3cb85c23de12ca29accc
tonafranco <tona.fran@hotmail.com>
2022-06-03 23:08:02 -0500

    Reto Firebase

--- app/src/main/java/com/movilesi/practicafirebase20/MainActivity.kt
+++ app/src/main/java/com/movilesi/practicafirebase20/MainActivity.kt
@@ -19,13 +19,20 @@ class MainActivity : AppCompatActivity() {
         myRef.child("ejemplo").child("2").setValue(Card("9","rojo","treboles"))
         myRef.child("ejemplo").child("3").setValue(Card("3","rojo","diamantes"))
         myRef.child("ejemplo").child("4").setValue(Card("5","negro","espadas"))
-        */
+
 
         myRef.child("users").child("user_id:01").setValue(User("tona","franco",28,"tona@hotmail.com"))
         myRef.child("users").child("user_id:02").setValue(User("alex","tapia",26,"alex@hotmail.com"))
         myRef.child("users").child("user_id:03").setValue(User("ema","gonzalez",25,"ema@hotmail.com"))
 
-        myRef.child("ejemplo").child("4").get().addOnSuccessListener { response ->
+         */
+
+        myRef.child("movies").child("1").setValue(Pelis("Back to the future", "1985", "tt0088763", "movie", "https://filmartgallery.com/products/back-to-the-future-4", "EU", "Comedia/Ciencia ficción"))
+        myRef.child("movies").child("2").setValue(Pelis("Star Wars Episode: VI", "1983", "tt0086190", "movie", "https://www.pinterest.es/pin/289778557267244296/?send=true", "EU", "\n" +
+                "Acción/Aventura/Fantasía/Ciencia ficción"))
+        myRef.child("movies").child("3").setValue(Pelis("Jurassic Park","1993","tt0107290","movie","https://moviepostermexico.com/products/poster-pelicula-jurassic-park","EU","Accion/Aventura/Ciencia ficción"))
+
+        myRef.child("movies").child("1").get().addOnSuccessListener { response ->
             //TODO value
             Log.d("firebaseResponse", response.value.toString())
         }.addOnFailureListener {
---
+++ app/src/main/java/com/movilesi/practicafirebase20/Pelis.kt
@@ -0,0 +1,11 @@
+package com.movilesi.practicafirebase20
+
+class Pelis (titulo: String, anio: String, imdbid: String, tipo: String, poster: String, pais: String, genero: String) {
+    val titulo = titulo
+    val anio = anio
+    val imdbid = imdbid
+    val tipo = tipo
+    val poster = poster
+    val pais = pais
+    val genero = genero
+}
